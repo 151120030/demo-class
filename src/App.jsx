af4a02fd31d39251cf3980501063eb1bc36a669c
Your Name <hiralkumbhani20@gmail.com>
2025-09-01 17:00:03 +0530

    x to yu

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
   return (
     <div>
       <input type="text" onChange={print} />
-      <button onClick={addTask}>Add x</button>
+      <button onClick={addTask}>Add yu</button>
 
       <ol>
         {tasks.map((t, index) => {
