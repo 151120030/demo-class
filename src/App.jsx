e4bf7fd9c1da29687c581f25f8b441364189e349
Your Name <hiralkumbhani20@gmail.com>
2025-09-01 15:56:06 +0530

    react setup

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const print = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const addTask = () => {
+    const updateTasks = [...tasks, inputValue];
+    setTasks(updateTasks);
+  };
+
+  const handleDelete = (index) => {
+    const updated = tasks.filter((t, idx) => {
+      return idx != index;
+    });
+
+    setTasks(updated);
+  };
+
+  return (
+    <div>
+      <input type="text" onChange={print} />
+      <button onClick={addTask}>Add</button>
+
+      <ol>
+        {tasks.map((t, index) => {
+          return (
+            <li key={index}>
+              {t}{" "}
+              <button
+                onClick={() => {
+                  handleDelete(index);
+                }}>
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ol>
+    </div>
+  );
+};
+
+export default App;
